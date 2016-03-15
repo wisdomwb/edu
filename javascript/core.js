929869dadcb6c467d46f7631cad63a0e81d0492c
weizhen <952465925@qq.com>
2016-03-16 06:26:55 +0800

    debug

--- javascript/core.js
+++ javascript/core.js
@@ -207,7 +207,7 @@ cancel.addEventListener('click',function  () {
 var hotList=$('j-hotlist').getElementsByTagName('li');
 get('http://study.163.com/webDev/hotcouresByCategory.htm',{},function (data) {
 	for (var i = 0; i < hotList.length; i++) {
-		alert(data);
+		alert(data[0]);
 		// hotList[i].getElementsByTagName('img')[0].src=data[10-i]['smallPhotoUrl'];
 	}
 })
